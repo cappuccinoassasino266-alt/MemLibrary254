8315b974d43b6a07c4f18edc9bce4596e9c53195
Student_6 <codecat.ua@gmail.com>
2026-05-16 17:07:12 +0300

    added List<Media Items>

--- src/main/java/com/example/memlibrary/HelloController.java
+++ src/main/java/com/example/memlibrary/HelloController.java
@@ -10,6 +10,9 @@ import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloController {
 
     @FXML
@@ -22,7 +25,22 @@ public class HelloController {
     VBox iconContainer;
 
     MediaPlayer mediaPlayer;
-    
+
+    private final List<MediaItem> items = new ArrayList<>();
+
+    private void createItems() {
+        items.add(new MediaItem("Штани за 40 гривень",
+                "/images/40grn.png",
+                "Штани за 40 гривень — український інтернет-мем, що виник у 2014 році та символізує роздратування від пошкодження чи втрати дешевої, але цінної та надійної речі. Фраза часто використовується для вираження комічної фрустрації щодо дрібних невдач у повсякденному житті, підкреслюючи економічну реальність багатьох українців, де недорогі покупки з секонд-хендів чи ринків набувають особливої ваги",
+                "/audio/40grn.mp3"));
+        items.add(new MediaItem("9 чи 10",
+                "/images/9chi10.png",
+                "Мем «9 чи 10, не чує баба» — це популярний український інтернет-мем, що базується на гумористичному аудіозаписі телефонної розмови між диспетчером поліції та літньою жінкою, яка недочуває.",
+                "/audio/9chi10.mp3"));
+    }
+
+
+
     public void iconButton1Clicked(ActionEvent event) {
 
     }
@@ -39,7 +57,7 @@ public class HelloController {
             mediaPlayer = new MediaPlayer(media);
             mediaPlayer.play();
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
